9e6aef34b7be110d7d6089baa0babe6bca4c9d84
Christoph Koschel <christoph.koschel86@gmail.com>
2026-01-15 19:37:33 +0100

    Better positioning

--- src/views/ProviderView.vue
+++ src/views/ProviderView.vue
@@ -9,27 +9,29 @@ const vm: ProviderViewModel = useViewModel(ProviderViewModel);
 </script>
 
 <template>
-    <div class="container m-auto">
-        <div class="fixed bottom-5 right-5">
+    <div class="relative container flex justify-center items-center h-1/2 m-auto">
+        <div class="absolute bottom-5 right-5">
             <button class="btn btn-lg btn-neutral btn-soft btn-square">
                 <LucideSettings/>
             </button>
         </div>
-        <h1 class="text-3xl text-center">
-            <Text :target="I18n.ProviderView.title" />
-        </h1>
-        <div class="flex justify-center pt-4">
-            <div class="join">
-                <div class="join-item card bg-base-100 card-border border-base-300 hover:bg-primary/50"
-                     @click="vm.onAniworldBtn">
-                    <div class="card-body">
-                        <img src="/aniworld-dark.svg" alt="Aniworld" class="w-[250px]"/>
+        <div>
+            <h1 class="text-3xl text-center">
+                <Text :target="I18n.ProviderView.title"/>
+            </h1>
+            <div class="flex justify-center pt-4">
+                <div class="join">
+                    <div class="join-item card bg-base-100 card-border border-base-300 hover:bg-primary/50"
+                         @click="vm.onAniworldBtn">
+                        <div class="card-body">
+                            <img src="/aniworld-dark.svg" alt="Aniworld" class="w-[250px]"/>
+                        </div>
                     </div>
-                </div>
-                <div class="join-item card bg-base-100 card-border border-base-300 hover:bg-primary/50"
-                     @click="vm.onStoBtn">
-                    <div class="card-body">
-                        <img src="/sto.svg" alt="Sto" class="w-[250px]"/>
+                    <div class="join-item card bg-base-100 card-border border-base-300 hover:bg-primary/50"
+                         @click="vm.onStoBtn">
+                        <div class="card-body">
+                            <img src="/sto.svg" alt="Sto" class="w-[250px]"/>
+                        </div>
                     </div>
                 </div>
             </div>
