ebcecc265350cf247456838bea11e83b16a22b35
Christoph Koschel <christoph.koschel86@gmail.com>
2026-04-19 14:35:49 +0200

    Add and remove series from custom lists

---
+++ src/icons/LucideHeart.vue
@@ -0,0 +1,13 @@
+<!--
+This file was generated with the UNOFFICIAL Iconify CLI v1.2.1
+
+Icon: lucide:heart
+Collection: Lucide
+License: ISC (ISC)
+Version: N/A
+Author: Lucide Contributors (https://github.com/lucide-icons/lucide)
+Downloaded: 2026-04-19T11:45:49.514Z
+-->
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676a.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"/></svg>
+</template>
