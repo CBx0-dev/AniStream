401105425b1557a63571c82ded311b69f8eba39f
Christoph Koschel <christoph.koschel86@gmail.com>
2026-04-22 12:35:20 +0200

    Reformat file

--- server/AniStream/DTO/SeriesFilterModel.cs
+++ server/AniStream/DTO/SeriesFilterModel.cs
@@ -3,6 +3,7 @@ namespace AniStream.API.DTO;
 public sealed class SeriesFilterModel
 {
     public required int Offset { get; set; }
+    
     public required int Limit { get; set; }
 
     public required string? SearchText { get; set; }
