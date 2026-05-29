6a09fef7c53ffef07e92dd3e857ef19e27867d48
Christoph Koschel <christoph.koschel86@gmail.com>
2026-05-29 21:59:51 +0200

    Open devTools automatically in devMode

--- app/src-tauri/src/lib.rs
+++ app/src-tauri/src/lib.rs
@@ -64,6 +64,16 @@ pub fn run() {
                 .set_focus();
         }));
     }
+    #[cfg(debug_assertions)]
+    {
+        builder = builder.setup(|app| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .open_devtools();
+            Ok(())
+        });
+    }
 
     builder.invoke_handler(tauri::generate_handler![report_issue])
         .run(tauri::generate_context!())
