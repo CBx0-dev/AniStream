72e3692b6acf54a74da46bb5c386bfac9c80a45b
Christoph Koschel <christoph.koschel86@gmail.com>
2026-06-27 14:44:51 +0200

    Implement verification logic

--- server/AniStream/Services/CredentialsService.cs
+++ server/AniStream/Services/CredentialsService.cs
@@ -1,14 +1,17 @@
+using System.Security.Cryptography;
+using System.Text;
 using AniStream.Contracts;
 using AniStream.Models;
+using Konscious.Security.Cryptography;
 
 namespace AniStream.API.Serivces;
 
 public sealed class CredentialsService : ICredentialsService
 {
     private readonly IUserService _userService;
-    private readonly IHttpContextAccessor  _context;
+    private readonly IHttpContextAccessor _context;
 
-    public CredentialsService(IUserService userService, IHttpContextAccessor  context)
+    public CredentialsService(IUserService userService, IHttpContextAccessor context)
     {
         _userService = userService;
         _context = context;
@@ -22,7 +25,11 @@ public sealed class CredentialsService : ICredentialsService
             return null;
         }
 
-        // TODO check password
+        if (!VerifyPassword(password, profile.Password, profile.PasswordSalt))
+        {
+            return null;
+        }
+
         return profile;
     }
 
@@ -35,5 +42,28 @@ public sealed class CredentialsService : ICredentialsService
         }
 
         return Task.FromResult(guid);
+    }
+
+    private bool VerifyPassword(string input, string passwordB64, string saltB64)
+    {
+        if (input.Length == 0)
+        {
+            return false;
+        }
+        
+        byte[] salt = Convert.FromBase64String(saltB64);
+        byte[] password = Convert.FromBase64String(passwordB64);
+
+        Argon2id argon2 = new Argon2id(Encoding.UTF8.GetBytes(input))
+        {
+            Salt = salt,
+            DegreeOfParallelism = 4,
+            MemorySize = 1024 * 1024,
+            Iterations = 3
+        };
+
+        byte[] computedHash = argon2.GetBytes(32);
+
+        return CryptographicOperations.FixedTimeEquals(computedHash, password);
     }
 }
